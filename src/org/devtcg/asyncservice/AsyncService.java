c6f339ff35282d5f5ffb4356d9c2a46ba725e6d8
Oleksandr Shevchenko <shevchenko.adb@gmail.com>
2011-08-02 13:52:00 +0200

    Add source files.

---
+++ src/org/devtcg/asyncservice/AsyncService.java
@@ -0,0 +1,91 @@
+package org.devtcg.asyncservice;
+
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.DeadObjectException;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.util.Log;
+
+public class AsyncService extends Service
+{
+    private static final String TAG = "AsyncService";
+
+    @Override
+    public void onCreate()
+    {
+        Log.d(TAG, "onCreate");
+    }
+
+    @Override
+    public void onDestroy()
+    {
+        /* TODO: Of course we would need to clean up. */
+        Log.d(TAG, "onDestroy");
+    }
+
+    public IBinder getBinder()
+    {
+        return mBinder;
+    }
+
+    private final IAsyncService.Stub mBinder = new IAsyncService.Stub()
+    {
+        public void startCount(final int to, final IAsyncServiceCounter callback)
+        {
+            Thread t = new Thread()
+            {
+                /* Survives interruption, but not otherwise more precise. */
+                public void preciseSleep(long millis)
+                {
+                    long endTime = System.currentTimeMillis() + millis;
+
+                    do {
+                        try
+                        {
+                            Thread.sleep(endTime - System.currentTimeMillis());
+                        }
+                        catch (InterruptedException e)
+                        {}
+                    } while (System.currentTimeMillis() < endTime);
+                }
+
+                public void run()
+                {
+                    for (int i = 1; i <= to; i++)
+                    {
+                        preciseSleep(1000);
+
+                        try
+                        {
+                            callback.handleCount(i);
+                        }
+                        catch (DeadObjectException e)
+                        {
+                            Log.d(TAG, "Dead peer, aborting...", e);
+                            break;
+                        } catch (RemoteException e) {
+                            // TODO Auto-generated catch block
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            };
+
+            t.start();
+        }
+    };
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // Select the interface to return.  If your service only implements
+        // a single interface, you can just return it here without checking
+        // the Intent.
+        if (IAsyncService.class.getName().equals(intent.getAction())) {
+            return mBinder;
+        }
+        return null;
+    }
+
+}
